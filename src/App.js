da3889b6c432e9278bfa99ec69950f183fea77d3
gshapovalov <grygorii.shapovalov@avid.com>
2022-05-08 12:27:45 +0300

    some changes #2

--- src/App.js
+++ src/App.js
@@ -107,16 +107,19 @@ function App() {
             <div className='mt40'>...Minting HOMEUA</div>
       }
       {
-        nftContract?.to &&
+        nftContract?.from &&
         <div className='mt40'>
           <div>HOMEUA minting successful!!!!</div>
           <a href={`https://testnets.opensea.io/${nftContract.from}`}>Click here</a>
           <span> to view your NFT on OpenSea.</span>
         </div>
       }
-      <div className='mt40'>
-        <a href={`https://rinkeby.etherscan.io/tx/${nftContract.hash}`}>Your transaction</a>
-      </div>
+      {
+        nftContract?.hash &&
+        <div className='mt40'>
+          <a href={`https://rinkeby.etherscan.io/tx/${nftContract?.hash}`}>Your transaction</a>
+        </div>
+      }
       <div className='mt40'>
         <div>SMART CONTRACT ADDRESS:</div>
         <a href={`https://rinkeby.etherscan.io/address/${contractAddress}`}>{contractAddress}</a>
