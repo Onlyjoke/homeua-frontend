663f5f225d7e08f99465248a801d307e18ca097c
gshapovalov <grygorii.shapovalov@avid.com>
2022-05-13 22:11:10 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,193 +1,224 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import axios from 'axios';
+
 import './App.css';
 import contract from './contracts/HomeUaTestNFT4.json';
-import { ethers } from 'ethers';
+import {ethers} from 'ethers';
 import gifHomeUa from './images/homeua.gif'
+import CounterInput from "react-counter-input";
 
 const contractAddress = "0xB2d494DCbEC3Aa221Fcf11E51B5718EE52484977";
 const abi = contract.abi;
+const price = 0.05;
 
 function App() {
-  const [currentAccount, setCurrentAccount] = useState(null);
-  const [isMinting, setMinting] = useState(false);
-  const [nftContract, setNftContract] = useState(null);
-  const [mintedNFT, setMintedNFT] = useState(null);
-  const [error, setTxError] = useState(null);
+    const [currentAccount, setCurrentAccount] = useState(null);
+    const [isMinting, setMinting] = useState(false);
+    const [nftContract, setNftContract] = useState(null);
+    const [mintedNFT, setMintedNFT] = useState(null);
+    const [error, setTxError] = useState(null);
+    const [mintCounter, changeMintCounter] = useState(1);
 
-  useEffect(() => {
-    checkWalletIsConnected();
-  }, [])
+    useEffect(() => {
+        checkWalletIsConnected();
+    }, [])
 
-  const checkWalletIsConnected = async () => {
-    const { ethereum } = window;
+    const checkWalletIsConnected = async () => {
+        const {ethereum} = window;
 
-    if (!ethereum) {
-      console.log("Make sure you have Metamask installed!");
-      return;
-    } else {
-      console.log("Wallet exists! We're ready to go!")
+        if (!ethereum) {
+            console.log("Make sure you have Metamask installed!");
+            return;
+        } else {
+            console.log("Wallet exists! We're ready to go!")
+        }
+
+        const accounts = await ethereum.request({method: 'eth_accounts'});
+
+        if (accounts.length !== 0) {
+            const account = accounts[0];
+            console.log("Found an authorized account: ", account);
+            setCurrentAccount(account);
+        } else {
+            console.log("No authorized account found");
+        }
     }
 
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    const connectWalletHandler = async () => {
+        const {ethereum} = window;
 
-    if (accounts.length !== 0) {
-      const account = accounts[0];
-      console.log("Found an authorized account: ", account);
-      setCurrentAccount(account);
-    } else {
-      console.log("No authorized account found");
-    }
-  }
+        if (!ethereum) {
+            alert("Please install Metamask!");
+        }
 
-  const connectWalletHandler = async () => {
-    const { ethereum } = window;
-
-    if (!ethereum) {
-      alert("Please install Metamask!");
+        try {
+            const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+            console.log("Found an account! Address: ", accounts[0]);
+            setCurrentAccount(accounts[0]);
+        } catch (err) {
+            console.log(err)
+        }
     }
 
-    try {
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-      console.log("Found an account! Address: ", accounts[0]);
-      setCurrentAccount(accounts[0]);
-    } catch (err) {
-      console.log(err)
+    const mintNftHandler = async () => {
+        try {
+            const {ethereum} = window;
+
+            if (ethereum) {
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const signer = provider.getSigner();
+                const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+                console.log("Initialize payment");
+                let nftTxn = await nftContract.mint(mintCounter, {
+                    value: ethers.utils.parseEther((mintCounter * price).toString())
+                });
+                setMinting(true);
+                console.log("Mining... please wait");
+                let tx = await nftTxn.wait();
+                setMinting(false);
+                console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+                setNftContract(nftTxn)
+                let event = tx.events[0]
+                let value = event.args[2]
+                let tokenId = value.toNumber()
+                console.log('tx', tx)
+                console.log('event', event)
+                console.log('event', value)
+                console.log('tokenId', tokenId)
+                console.log('WWW signer WWW', signer)
+
+
+                await getMintedNFT(tokenId)
+
+            } else {
+                console.log("Ethereum object does not exist");
+            }
+
+        } catch (err) {
+            console.log(err);
+        }
     }
-  }
 
-  const mintNftHandler = async () => {
-    try {
-      const { ethereum } = window;
+    // Gets the minted NFT data
+    const getMintedNFT = async (tokenId) => {
+        try {
+            const {ethereum} = window
+            if (ethereum) {
+                const provider = new ethers.providers.Web3Provider(ethereum)
+                const signer = provider.getSigner()
+                const nftContract = new ethers.Contract(
+                    contractAddress,
+                    abi,
+                    signer
+                )
 
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+                let tokenUri = await nftContract.tokenURI(tokenId)
+                const ipfsToHTTP = tokenUri.replace("ipfs://", "https://ipfs.io/ipfs/");
+                let {data} = await axios.get(ipfsToHTTP)
+                const image = data?.image.replace("ipfs://", "https://ipfs.io/ipfs/");
+                data.image = image;
 
-        console.log("Initialize payment");
-        let nftTxn = await nftContract.mint(1, { value: ethers.utils.parseEther("0.05") });
-        setMinting(true);
-        console.log("Mining... please wait");
-        let tx = await nftTxn.wait();
-        setMinting(false);
-        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
-        setNftContract(nftTxn)
-        let event = tx.events[0]
-        let value = event.args[2]
-        let tokenId = value.toNumber()
-
-        await getMintedNFT(tokenId)
-
-      } else {
-        console.log("Ethereum object does not exist");
-      }
-
-    } catch (err) {
-      console.log(err);
+                setMintedNFT(data)
+            } else {
+                console.log("Ethereum object doesn't exist!")
+            }
+        } catch (error) {
+            console.log(error)
+            setTxError(error.message)
+        }
     }
-  }
 
-  // Gets the minted NFT data
-  const getMintedNFT = async (tokenId) => {
-    try {
-      const { ethereum } = window
-      console.log('tokenId ', tokenId)
-
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const nftContract = new ethers.Contract(
-            contractAddress,
-            abi,
-            signer
+    const connectWalletButton = () => {
+        return (
+            <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+                Connect Wallet
+            </button>
         )
-
-        let tokenUri = await nftContract.tokenURI(tokenId)
-        const ipfsToHTTP = tokenUri.replace("ipfs://", "https://ipfs.io/ipfs/");
-        let { data } = await axios.get(ipfsToHTTP)
-        const image = data?.image.replace("ipfs://", "https://ipfs.io/ipfs/");
-        data.image = image;
-
-        setMintedNFT(data)
-      } else {
-        console.log("Ethereum object doesn't exist!")
-      }
-    } catch (error) {
-      console.log(error)
-      setTxError(error.message)
     }
-  }
 
-  const connectWalletButton = () => {
+    const mintNftButton = () => {
+        return (
+            <>
+                <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
+                    Mint NFT
+                </button>
+                <div className='mint-nft-counter'>
+                    <CounterInput
+                        min={1}
+                        max={5}
+                        onCountChange={count => changeMintCounter(count)}
+                    />
+                </div>
+            </>
+        )
+    }
+
     return (
-      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
-        Connect Wallet
-      </button>
-    )
-  }
-
-  const mintNftButton = () => {
-    return (
-      <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
-        Mint NFT
-      </button>
-    )
-  }
-
-  return (
-    <div className='main-app'>
-      <h1>HOMEUA MINT</h1>
-      <div className='mt40'>
-        {currentAccount ? mintNftButton() : connectWalletButton()}
-      </div>
-      {
-        isMinting &&
-          <div className='mt40'>
-            <div>...Minting HOMEUA</div>
+        <div className='main-app'>
+            <h1>HOMEUA MINT</h1>
             <div className='mt40'>
-              <img
-                  src={gifHomeUa}
-                  alt='minting HOMEUA NFT'
-              />
+                {currentAccount ? mintNftButton() : connectWalletButton()}
             </div>
-          </div>
-      }
-      {
-        mintedNFT &&
-          <div className='mt40'>
-            <div>NAME: {mintedNFT?.name}</div>
-            <div className='minted-img-wrapp mt40'>
-              <img
-                  src={mintedNFT?.image}
-                  alt='minted HOMEUA NFT'
-              />
-            </div>
+            {
+                error &&
+                <div>
+                    {error.message}
+                </div>
+            }
+            {
+                isMinting &&
+                <div className='mt40'>
+                    <div className='blink_me'>...Minting HOMEUA</div>
+                    <div className='mt40'>
+                        <img
+                            src={gifHomeUa}
+                            alt='minting HOMEUA NFT'
+                        />
+                    </div>
+                </div>
+            }
+            {
+                mintedNFT &&
+                <div className='mt40'>
+                    <div>NAME: {mintedNFT?.name}</div>
+                    <div className='mt40'>
+                        <a
+                            className='minted-img-wrapp'
+                            href={`https://testnets.opensea.io/assets/${contractAddress}/${mintedNFT?.edition}`}
+                            target='_blank' rel="noreferrer"
+                        >
+                            <img
+                                src={mintedNFT?.image}
+                                alt='minted HOMEUA NFT'
+                            />
+                        </a>
+                    </div>
+                    <div className='mt40'>
+                        <div>DESCRIPTION: {mintedNFT?.description}</div>
+                    </div>
+                </div>
+            }
+            {
+                nftContract?.from &&
+                <div className='mt40'>
+                    <div>HOMEUA minting successful!!!!</div>
+                    <a target='_blank' rel="noreferrer"   href={`https://testnets.opensea.io/${nftContract.from}`}>Click here</a>
+                    <span> to view your NFT on OpenSea.</span>
+                </div>
+            }
+            {
+                nftContract?.hash &&
+                <div className='mt40'>
+                    <a target='_blank' rel="noreferrer"   href={`https://rinkeby.etherscan.io/tx/${nftContract?.hash}`}>Your transaction</a>
+                </div>
+            }
             <div className='mt40'>
-              <div>DESCRIPTION: {mintedNFT?.description}</div>
+                <div>SMART CONTRACT ADDRESS:</div>
+                <a target='_blank' rel="noreferrer"   href={`https://rinkeby.etherscan.io/address/${contractAddress}`}>{contractAddress}</a>
             </div>
-          </div>
-      }
-      {
-        nftContract?.from &&
-        <div className='mt40'>
-          <div>HOMEUA minting successful!!!!</div>
-          <a href={`https://testnets.opensea.io/${nftContract.from}`}>Click here</a>
-          <span> to view your NFT on OpenSea.</span>
         </div>
-      }
-      {
-        nftContract?.hash &&
-        <div className='mt40'>
-          <a href={`https://rinkeby.etherscan.io/tx/${nftContract?.hash}`}>Your transaction</a>
-        </div>
-      }
-      <div className='mt40'>
-        <div>SMART CONTRACT ADDRESS:</div>
-        <a href={`https://rinkeby.etherscan.io/address/${contractAddress}`}>{contractAddress}</a>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default App;
